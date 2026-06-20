288da0b9d20cacc2a6e9a590eec4444a459dc97e
Felip8 <valdivinobiava496@gmail.com>
2026-06-20 16:31:39 -0300

    aaaaaaa

--- src/Pages/Characters.tsx
+++ src/Pages/Characters.tsx
@@ -1,7 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import "../css/characters/character.css";
+import Pig from "../assets/logo.png";
+
 export default function Characters() {
+  const navigate = useNavigate();
+
+  const voltar = () => {
+    navigate("/criarCronograma");
+  };
+
   return (
-    <div>
-      <p>teste</p>
+    <div className="characters-page">
+      <button onClick={voltar} className="button-voltar">
+        Voltar
+      </button>
+
+      <div className="characters-container">
+        <div className="character-card">
+          <p className="container-centraliza-p">Pig</p>
+          <img src={Pig} alt="Pig" className="pig" />
+          <p>Current character</p>
+        </div>
+
+        <div className="add-character">
+          <button> + </button>
+        </div>
+      </div>
     </div>
   );
 }
